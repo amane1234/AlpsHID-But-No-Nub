c686f1e77a7782f5a7ecd64af4062f80e8e26fce
캬루애호가 <47608969+amane1234@users.noreply.github.com>
2025-01-05 01:59:27 -0500

    Update helpers.hpp

--- AlpsHID/helpers.hpp
+++ AlpsHID/helpers.hpp
@@ -1,11 +1,3 @@
-//
-//  helpers.hpp
-//  VoodooI2C
-//
-//  Created by Alexandre on 31/07/2017.
-//  Copyright © 2017 Alexandre Daoud. All rights reserved.
-//
-
 #ifndef helpers_hpp
 #define helpers_hpp
 
@@ -16,30 +8,38 @@
 #define EXPORT __attribute__((visibility("default")))
 #endif
 
+// Power management constants
 #define kIOPMPowerOff                       0
 #define kVoodooI2CIOPMNumberPowerStates     2
 
+// Bitwise manipulation macro
 #define BIT(nr) (1UL << (nr))
 
+// Function declarations
 UInt16 abs(SInt16 x);
-
 const char* getMatchedName(IOService* provider);
 
+// Safe function to set a dictionary value as an OSNumber
 inline void setOSDictionaryNumber(OSDictionary* dictionary, const char * key, UInt32 number) {
+    // Create a new OSNumber object with the specified number
     if (OSNumber* os_number = OSNumber::withNumber(number, 32)) {
+        // Add the OSNumber to the dictionary with the specified key
         dictionary->setObject(key, os_number);
-        os_number->release();
+        // Safely release the object after use
+        os_number->safeRelease();
     }
 }
 
+// Enum to represent the I2C state (on/off)
 enum VoodooI2CState {
-    kVoodooI2CStateOff = 0,
-    kVoodooI2CStateOn = 1
+    kVoodooI2CStateOff = 0,  // I2C is off
+    kVoodooI2CStateOn  = 1   // I2C is on
 };
 
+// Power state structure for IOPowerManagement
 static IOPMPowerState VoodooI2CIOPMPowerStates[kVoodooI2CIOPMNumberPowerStates] = {
-    {1, kIOPMPowerOff, kIOPMPowerOff, kIOPMPowerOff, 0, 0, 0, 0, 0, 0, 0, 0},
-    {1, kIOPMPowerOn, kIOPMPowerOn, kIOPMPowerOn, 0, 0, 0, 0, 0, 0, 0, 0}
+    {1, kIOPMPowerOff, kIOPMPowerOff, kIOPMPowerOff, 0, 0, 0, 0, 0, 0, 0, 0},  // Power off state
+    {1, kIOPMPowerOn,  kIOPMPowerOn,  kIOPMPowerOn,  0, 0, 0, 0, 0, 0, 0, 0}   // Power on state
 };
 
 #endif /* helpers_hpp */
